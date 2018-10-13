2138a992523243e28fb78f76b3ecb3ccddd919fd
KattieM <kattysmiley@gmail.com>
2018-10-13 15:00:55 +0200

    --initial commit

---
+++ src/main/java/com/code/to/vote/codeToVote/CodeToVoteApplication.java
@@ -0,0 +1,12 @@
+package com.code.to.vote.codeToVote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeToVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeToVoteApplication.class, args);
+	}
+}
